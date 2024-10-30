d00c022a92de063ac606d84aa060216f5522fd1a
lotok14 <98856437+lotok14@users.noreply.github.com>
2024-10-30 16:46:46 +0100

    Add files via upload

---
+++ 5dt_zad5/sklep.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styll.css">
+    <title>Hurtownia</title>
+</head>
+<body>
+    <div class="logo">
+    <img src="komputer.png" alt="hurtownia komputerowa">
+    </div>
+    <div class="lista">
+    <ol style="list-style-type: inherit">
+    <li>Sprzęt</li>
+        <ul style="list-style-type: decimal">
+            <li>Procesory</li>
+            <li>Pamięci RAM</li>
+            <li>Monitory</li>
+            <li>Obudowy</li>
+            <li>Karty graficzne</li>
+            <li>Dyski twarde</li>
+    </ul>
+    <li>Oprogramowanie</li>
+    </ol>
+    </div>
+    <div class="formularz">
+        <h2>Hurtownia komputerowa</h2>
+        <form method="POST" action="">
+        <input type="number">
+        <input type="submit" value="SPRAWDŹ">
+    </form>
+    </div>
+    <div class="glowny">
+        <h1>Podzespoły we wskazanej kategorii</h1>
+    </div>
+    <div class="stopka">
+    <h3>Hurtownia działa od poniedziałku do soboty w godzinach 7<sup>00</sup>-16<sup>00</sup></h3>
+    <a href="mailto:bok@hurtownia.pl">Napisz do nas</a>
+    <h>Partnerzy: </h>
+    <a href="http://intel.p" target="_blank">Intel</a>
+    <a href="http://amd.pl" target="_blank">AMD</a>
+    <p>Stronę wykonał: 05261012339</p>
+    </div>
+
+</body>
+</html>
